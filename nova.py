2f72469ab1bf9ee35236b07fd28488c6ff121e42
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-11-20 20:11:54 -0500

    simple heading / velocity and starfield movement

--- nova.py
+++ nova.py
@@ -2,6 +2,7 @@
 # coding: latin_1
 
 import libtcodpy as libtcod
+import math
 from random import randrange, choice
 
 SCREEN_WIDTH = 118
@@ -33,43 +34,50 @@ key = libtcod.Key()
 
 class Starfield:
     def __init__(self):
-        self.stars = [ [randrange(0, SCREEN_WIDTH), randrange(0, SCREEN_HEIGHT), choice([1,2,3])] for i in range(0, MAX_STARS) ]
+        self.parallax_speeds = [0.3, 0.6, 1.0]
+        self.stars = [
+            [float(randrange(0, SCREEN_WIDTH)), float(randrange(0, SCREEN_HEIGHT)), choice(self.parallax_speeds)]
+                for i in range(0, MAX_STARS) ]
 
     def __getitem__(self, index):
         return self.stars[index]
 
-    def scroll(self, heading=0):
-        if heading == 0:
-            deltax = 1
-            deltay = 0
-        elif heading == 90:
-            deltax = 0
-            deltay = 1
-        elif heading == 180:
-            deltax = -1
-            deltay = 0
-        elif heading == 270:
-            deltax = 0
-            deltay = -1
+    def scroll(self, heading=0.0, velocity=0.0):
+        # if heading == 0:
+        #     deltax = 1.0
+        #     deltay = 0.0
+        # elif heading == 90:
+        #     deltax = 0.0
+        #     deltay = 1.0
+        # elif heading == 180:
+        #     deltax = -1.0
+        #     deltay = 0.0
+        # elif heading == 270:
+        #     deltax = 0.0
+        #     deltay = -1.0
+
+        deltax = math.cos(heading) * velocity
+        deltay = math.sin(heading) * velocity
+
         for star in self.stars:
             star[0] += deltax * star[2]
             star[1] += deltay * star[2]
-            if star[0] >= SCREEN_WIDTH:
+            if star[0] >= SCREEN_WIDTH-1:
                 star[0] = 0
-                star[1] = randrange(0, SCREEN_HEIGHT)
-                star[2] = choice([1,2,3])
+                star[1] = randrange(0, SCREEN_HEIGHT-1)
+                star[2] = choice(self.parallax_speeds)
             elif star[0] < 0:
                 star[0] = SCREEN_WIDTH-1
-                star[1] = randrange(0, SCREEN_HEIGHT)
-                star[2] = choice([1,2,3])
-            elif star[1] >= SCREEN_HEIGHT:
-                star[0] = randrange(0, SCREEN_WIDTH)
+                star[1] = randrange(0, SCREEN_HEIGHT-1)
+                star[2] = choice(self.parallax_speeds)
+            elif star[1] >= SCREEN_HEIGHT-1:
+                star[0] = randrange(0, SCREEN_WIDTH-1)
                 star[1] = 0
-                star[2] = choice([1,2,3])
+                star[2] = choice(self.parallax_speeds)
             elif star[1] < 0:
-                star[0] = randrange(0, SCREEN_WIDTH)
+                star[0] = randrange(0, SCREEN_WIDTH-1)
                 star[1] = SCREEN_HEIGHT-1
-                star[2] = choice([1,2,3])
+                star[2] = choice(self.parallax_speeds)
 
 starfield = Starfield()
 
@@ -77,8 +85,13 @@ class Ship:
     def __init__(self):
         self.x = SCREEN_WIDTH / 2
         self.y = SCREEN_HEIGHT / 2
-        self.heading = 0
-        self.velocity = 0
+
+        self.deltav = 1.0
+        self.turn_rate = math.radians(1.0)
+        self.twopi = 2 * math.pi
+
+        self.heading = 0.0
+        self.velocity = 0.0
         self.ship = [
             # r'  ^   ',
             # r' /#\  ',
@@ -90,6 +103,24 @@ class Ship:
             r' ÛÛÛ  ',
             r'ûÛVÛú ',
             r'üý üý ',]
+
+    def turn_left(self):
+        self.heading += self.turn_rate
+        if self.heading > self.twopi:
+            self.heading = 0.0
+
+    def turn_right(self):
+        self.heading -= self.turn_rate
+        if self.heading < 0.0:
+            self.heading = self.twopi
+
+    def increase_throttle(self):
+        self.velocity += self.deltav
+
+    def decrease_throttle(self):
+        self.velocity -= self.deltav
+        if self.velocity < 0:
+            self.velocity = 0.0
 
     def draw(self):
         for y, line in enumerate(self.ship):
@@ -110,13 +141,14 @@ objects = [player_ship]
 
 def render_all():
     for star in starfield:
-        if star[2] == 3:
+        color = 255
+        if star[2] > 0.9:
             color = 255
-        elif star[2] == 2:
+        elif 0.5 < star[2] < 0.7:
             color = 170
-        elif star[2] == 1:
+        elif 0.2 < star[2] < 0.4:
             color = 85
-        buffer.set_fore(star[0], star[1], color, color, color, 15)
+        buffer.set_fore(int(round(star[0])), int(round(star[1])), color, color, color, 15)
 
     for object in objects:
         object.draw()
@@ -127,7 +159,7 @@ def render_all():
     libtcod.console_set_default_foreground(panel_console, libtcod.white)
     libtcod.console_print_ex(panel_console, 0, 0, libtcod.BKGND_NONE, libtcod.LEFT,
         "Ship [Heading: {}]  [Velocity: {}]  [Position: {}, {}]".format(
-            player_ship.heading, player_ship.velocity, player_ship.x, player_ship.y) )
+            round(math.degrees(player_ship.heading),2), player_ship.velocity, player_ship.x, player_ship.y) )
 
     # panel_buffer.blit( panel_console )
     libtcod.console_blit(panel_console, 0, 0, HUD_WIDTH, HUD_HEIGHT, 0, 0, 0)
@@ -146,16 +178,18 @@ def handle_keys():
     # if game_state == 'playing':
     #     #movement keys
     if key.vk == libtcod.KEY_UP:
-        starfield.scroll(90)
-        # player_ship.y -= 1
+        player_ship.increase_throttle()
     elif key.vk == libtcod.KEY_DOWN:
-        starfield.scroll(270)
+        player_ship.decrease_throttle()
+        # starfield.scroll(270)
         # player_ship.y += 1
     elif key.vk == libtcod.KEY_LEFT:
-        starfield.scroll(0)
+        player_ship.turn_left()
+        # starfield.scroll(0)
         # player_ship.x -= 1
     elif key.vk == libtcod.KEY_RIGHT:
-        starfield.scroll(180)
+        player_ship.turn_right()
+        # starfield.scroll(180)
         # player_ship.x += 1
     #     else:
     #         #test for other keys
@@ -180,6 +214,10 @@ def handle_keys():
 
 while not libtcod.console_is_window_closed():
     libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS|libtcod.EVENT_MOUSE,key,mouse)
+
+    if player_ship.velocity > 0.0:
+        starfield.scroll( player_ship.heading, player_ship.velocity )
+
     render_all()
     libtcod.console_flush()
 
