6f456f9fc77d3c60415833917fa6860d7473c919
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-11-26 01:58:32 -0500

    experimenting with different ship sprites

--- images/generate_ship_sprites.sh
+++ images/generate_ship_sprites.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
-for angle in `seq 0 10 350`
-do
-  convert \
-    -size 16x16 xc:blue -fill white -stroke black -strokewidth 1.5 \
-    -draw "stroke-linecap round affine `./affine_rotate -$angle 7,7, 7,7`
-        polygon 2,1 15,7 2,13, 5,7" \
-    -fuzz 55% \
-    -fill blue \
-      -draw 'color 0,0 floodfill' \
-      -draw 'color 0,15 floodfill' \
-      -draw 'color 15,0 floodfill' \
-      -draw 'color 15,15 floodfill' \
-    `if (( $angle < 100 ))
-    then
-      echo ship_0$angle.png
-    else
-      echo ship_$angle.png
-    fi`
-done
-
-mv ship_00.png ship_000.png
-
-# for angle in `seq -w 0 10 350`
+# for angle in `seq 0 10 350`
 # do
 #   convert \
-#     -alpha set \
-#     -background black \
-#     -distort ScaleRotateTranslate -$angle \
-#     -fuzz 0 -fill blue \
+#     -size 16x16 xc:blue -fill white -stroke black -strokewidth 1.5 \
+#     -draw "stroke-linecap round affine `./affine_rotate -$angle 7,7, 7,7`
+#         polygon 2,1 15,7 2,13, 5,7" \
+#     -fuzz 55% \
+#     -fill blue \
 #       -draw 'color 0,0 floodfill' \
 #       -draw 'color 0,15 floodfill' \
 #       -draw 'color 15,0 floodfill' \
 #       -draw 'color 15,15 floodfill' \
-#     ship.png ship_$angle.png
-#     # -rotate -$angle \
-#     # -gravity NorthWest \
-#     # -crop 16x16+0+0 \
-#     # -transparent black \
+#     `if (( $angle < 100 ))
+#     then
+#       echo ship_0$angle.png
+#     else
+#       echo ship_$angle.png
+#     fi`
 # done
+
+# mv ship_00.png ship_000.png
+
+for angle in `seq -w 0 10 350`
+do
+  convert \
+    -alpha on \
+    -background transparent \
+    -rotate -$angle \
+    -gravity NorthWest \
+    -crop 16x16+0+0 \
+    rocket.png rocket_$angle.png
+    # -transparent-color blue \
+    # -fuzz 0 -fill blue \
+    #   -draw 'color 0,0 floodfill' \
+    #   -draw 'color 0,15 floodfill' \
+    #   -draw 'color 15,0 floodfill' \
+    #   -draw 'color 15,15 floodfill' \
+    # -distort ScaleRotateTranslate -$angle \
+done
 
