16637ac4eda57398df4554e2da98b99022d4be85
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-11-26 00:31:16 -0500

    add heigtmap generation to planet class

--- planet.py
+++ planet.py
@@ -1,6 +1,9 @@
 # import pprint
 # pp = pprint.PrettyPrinter(indent=4, width=200).pprint
+import math
 from random import randrange
+
+import libtcodpy as libtcod
 
 class Planet:
     def __init__(self, sector):
@@ -11,7 +14,54 @@ class Planet:
         if (self.width % 2) != 0:
             self.width += 1
         self.height = self.width
+
+        self.colormap = libtcod.color_gen_map(
+            [ libtcod.Color(0,0,0), libtcod.Color(30,30,170),
+              libtcod.Color(114, 150, 71), libtcod.Color(80,120,10),
+              libtcod.Color(17,109,7), libtcod.Color(120,220,120),
+              libtcod.Color(208,208,239), libtcod.Color(255,255,255)],
+            [ 0, 30, 34, 80, 90, 200, 210, 255]
+        )
         self.build_circle_mask()
+        self.build_heightmap()
+
+    def addHill(self, hm,nbHill,baseRadius,radiusVar,height) :
+        for i in range(nbHill) :
+            hillMinRadius=baseRadius*(1.0-radiusVar)
+            hillMaxRadius=baseRadius*(1.0+radiusVar)
+            radius = libtcod.random_get_float(self.rnd,hillMinRadius, hillMaxRadius)
+            theta = libtcod.random_get_float(self.rnd,0.0, 6.283185) # between 0 and 2Pi
+            dist = libtcod.random_get_float(self.rnd,0.0, float(min(self.width,self.height))/2 - radius)
+            xh = int(self.width/2 + math.cos(theta) * dist)
+            yh = int(self.height/2 + math.sin(theta) * dist)
+            libtcod.heightmap_add_hill(hm,float(xh),float(yh),radius,height)
+
+    def build_heightmap(self):
+        # self.rnd=libtcod.random_new_from_seed(1094911894)
+        self.rnd=libtcod.random_get_instance()
+        # 3x3 kernel for smoothing operations
+        smoothKernelSize=9
+        smoothKernelDx=[-1,0,1,-1,0,1,-1,0,1]
+        smoothKernelDy=[-1,-1,-1,0,0,0,1,1,1]
+        smoothKernelWeight=[1.0,2.0,1.0,2.0,20.0,2.0,1.0,2.0,1.0]
+
+        hm=libtcod.heightmap_new(self.width,self.height)
+        coef=[0.11,0.22,-0.22,0.18,]
+        tmp =libtcod.heightmap_new(self.width,self.height)
+        libtcod.heightmap_add_voronoi(tmp,100,4,coef,self.rnd)
+        libtcod.heightmap_normalize(tmp)
+        libtcod.heightmap_add_hm(hm,tmp,hm)
+        libtcod.heightmap_delete(tmp)
+        self.addHill(hm,40,11.4,0.31,0.09)
+        libtcod.heightmap_add(hm,-0.2)
+        libtcod.heightmap_clamp(hm,0.0,1.0)
+        smoothKernelWeight[4] = 20
+        for i in range(2,-1,-1) :
+            libtcod.heightmap_kernel_transform(hm,smoothKernelSize,smoothKernelDx,smoothKernelDy,smoothKernelWeight,0,0.86)
+        libtcod.heightmap_rain_erosion(hm,1000,0.46,0.12,self.rnd)
+        libtcod.heightmap_normalize(hm,0,255)
+        self.heightmap = hm
+        print(repr(libtcod.heightmap_get_minmax(self.heightmap)))
 
     def build_circle_mask(self):
         radius = self.width / 2
@@ -52,7 +102,13 @@ class Planet:
         for x in range(startingx, endingx):
             for y in range(startingy, endingy, -1):
                 if self.circle_mask[row][col]:
-                    self.sector.buffer.set_fore( x, self.sector.mirror_y_coordinate(y), 128, 255, 128, ord('@') )
+                    color = int(libtcod.heightmap_get_value(self.heightmap, row, col))
+                    if color > 255:
+                        color = 255
+                    self.sector.buffer.set(x, self.sector.mirror_y_coordinate(y),
+                        self.colormap[color][0], self.colormap[color][1], self.colormap[color][2],
+                        self.colormap[color][0], self.colormap[color][1], self.colormap[color][2], ord('@') )
+                        # 128, 255, 128, ord('@') )
                 col += 1
             col = start_col
             row += 1
