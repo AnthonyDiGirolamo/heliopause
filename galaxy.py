04812b26e458c6168aa66c7490a7c035714ff113
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-05-06 10:13:07 -0400

    Removed trace print statements, old code and force directed method from Galaxy

--- galaxy.py
+++ galaxy.py
@@ -14,7 +14,7 @@ from nebula import Nebula
 from starfield import Starfield
 
 class Galaxy:
-    def __init__(self, width, height, seed=39541358901):
+    def __init__(self, width, height, seed=1358901):
         self.screen_width = width
         self.screen_height = height
         self.seed = seed
@@ -29,7 +29,7 @@ class Galaxy:
         self.planet_name_index = -1
 
         # Build Galaxy Map
-        self.bsp_depth = 5
+        self.bsp_depth = 6
         self.bsp = libtcod.bsp_new_with_size(0, 0, self.screen_width, self.screen_height)
         libtcod.bsp_split_recursive(self.bsp, self.rnd, self.bsp_depth, 8, 8, 1.0, 1.0)
 
@@ -48,11 +48,9 @@ class Galaxy:
             self.new_sector()
 
         self.link_sectors()
-        # pp( [sector.neighbors for sector in self.sectors] )
 
         self.current_sector = random.randrange(self.sector_count)
-        # self.current_sector = 0
-        pp("total sectors: {}  current sector: {}".format(self.sector_count, self.current_sector))
+        # pp("total sectors: {}  current sector: {}".format(self.sector_count, self.current_sector))
         self.targeted_sector_index = 0
         self.selected_blink = 0
 
@@ -95,7 +93,7 @@ class Galaxy:
         # Link nodes in the bsp tree
         for i in range(self.bsp_depth):
             for index in range(0, self.sector_count, 2**(i+1)):
-                print("current depth: {} starting index: {}".format(i, index))
+                # print("current depth: {} starting index: {}".format(i, index))
                 node1_index = index
                 if i == 0:
                     # we are linking the lowest level nodes
@@ -124,31 +122,16 @@ class Galaxy:
                                     min_distance = d
                                     min_node1 = node1["index"]
                                     min_node2 = node2["index"]
-                                    print("new min: {} indexes: {} {}".format(d, min_node1, min_node2))
+                                    # print("new min: {} indexes: {} {}".format(d, min_node1, min_node2))
                                     node1_index = min_node1
                                     node2_index = min_node2
 
-                    print("done min ---")
+                    # print("done min ---")
 
                 if node2_index < self.sector_count:
                     print("linked {} -> {}".format(node1_index, node2_index))
                     if node2_index not in self.sectors[node1_index].neighbors:
                         self.sectors[node1_index].neighbors.append( node2_index )
-
-        # Circle
-        # for index, sector in enumerate(self.sectors):
-        #     if index == self.sector_count-1:
-        #         sector.neighbors.append( 0 )
-        #     else:
-        #         sector.neighbors.append( index+1 )
-
-        # Randomly
-        # for index, sector in enumerate(self.sectors):
-        #     while len(sector.neighbors) == 0:
-        #         for i in range(random.randrange(5)):
-        #             link = random.randrange(self.sector_count)
-        #             if index != link and link not in sector.neighbors and index not in self.sectors[link].neighbors:
-        #                 sector.neighbors.append( link )
 
         # Add links in the other direction
         for index, sector in enumerate(self.sectors):
@@ -164,84 +147,6 @@ class Galaxy:
 
         # pp([sector.neighbors for sector in self.sectors])
         # pp(self.one_way_links)
-
-    def force_directed(self):
-        repulsion_constant = 100
-        attraction_constant = 0.05
-        spring_length = 4
-
-        displacements = [[0, 0] for sector in self.sectors]
-
-        for index1, sector1 in enumerate(self.sectors):
-            for index2, sector2 in enumerate(self.sectors):
-                if index1 == index2:
-                    continue
-
-                proximity = max( math.sqrt((sector2.galaxy_position_x - sector1.galaxy_position_x)**2 + (sector2.galaxy_position_y - sector1.galaxy_position_y)**2), 1.0)
-                repulsion_force = -(repulsion_constant / proximity**2)
-
-                x_displacement = sector2.galaxy_position_x - sector1.galaxy_position_x
-                y_displacement = sector2.galaxy_position_y - sector1.galaxy_position_y
-                try:
-                    angle = math.atan(y_displacement / x_displacement)
-                except:
-                    angle = 0.0
-                if x_displacement > 0.0 and y_displacement < 0.0:
-                    angle += math.pi * 2
-                elif x_displacement < 0.0:
-                    angle += math.pi
-
-                displacements[index1][0] += math.cos(angle) * repulsion_force
-                displacements[index1][1] += math.sin(angle) * repulsion_force
-
-        for index1, index2 in self.one_way_links:
-            sector1 = self.sectors[index1]
-            sector2 = self.sectors[index2]
-
-            proximity = max( math.sqrt((sector2.galaxy_position_x - sector1.galaxy_position_x)**2 + (sector2.galaxy_position_y - sector1.galaxy_position_y)**2), 1.0)
-            attraction_force = attraction_constant * max(proximity - spring_length, 0)
-
-            x_displacement = sector2.galaxy_position_x - sector1.galaxy_position_x
-            y_displacement = sector2.galaxy_position_y - sector1.galaxy_position_y
-            try:
-                angle = math.atan(y_displacement / x_displacement)
-            except:
-                angle = 0.0
-            if x_displacement > 0.0 and y_displacement < 0.0:
-                angle += math.pi * 2
-            elif x_displacement < 0.0:
-                angle += math.pi
-
-            displacements[index1][0] += math.cos(angle) * attraction_force
-            displacements[index1][1] += math.sin(angle) * attraction_force
-
-        # Move sector positions
-        for index, sector in enumerate(self.sectors):
-            sector.galaxy_position_x += int(round(displacements[index][0]))
-            sector.galaxy_position_y += int(round(displacements[index][1]))
-
-        # Recenter Sector Map
-        min_x = min( [sector.galaxy_position_x for sector in self.sectors] )
-        min_y = min( [sector.galaxy_position_y for sector in self.sectors] )
-        max_x = max( [sector.galaxy_position_x for sector in self.sectors] )
-        max_y = max( [sector.galaxy_position_y for sector in self.sectors] )
-
-        width = max_x - min_x
-        height = max_y - min_y
-        disp_x = ((self.screen_width/2) - (width/2)) - min_x
-        disp_y = ((self.screen_height/2) - (height/2)) - min_y
-
-        for index, sector in enumerate(self.sectors):
-            sector.galaxy_position_x += disp_x
-            sector.galaxy_position_y += disp_y
-            if sector.galaxy_position_x < 2:
-                sector.galaxy_position_x = 2
-            if sector.galaxy_position_y < 2:
-                sector.galaxy_position_y = 2
-            if sector.galaxy_position_x > self.screen_width - 3:
-                sector.galaxy_position_x = self.screen_width - 3
-            if sector.galaxy_position_y > self.screen_height - 3:
-                sector.galaxy_position_y = self.screen_height - 3
 
 
     def draw(self, buffer):
@@ -261,14 +166,11 @@ class Galaxy:
             )
             x,y=libtcod.line_step()
             while x is not None:
-                # buffer.set_back(x, y, color[0], color[1], color[2])
-                # buffer.set(x, y, 0, 0, 0, color[0], color[1], color[2], ord('.'))
                 buffer.set_fore(x, y, color[0], color[1], color[2], 11)
                 x,y=libtcod.line_step()
 
         # Draw Sectors Nodes
         for index, sector in enumerate(self.sectors):
-            # color = [int(sector.nebula_background[0] * 255), int(sector.nebula_background[1] * 255), int(sector.nebula_background[2] * 255)]
             x, y = sector.galaxy_position_x, sector.galaxy_position_y
             buffer.set_fore(x, y, sector.star_color[0], sector.star_color[1], sector.star_color[2], sector.star_icon)
             for x, y, icon in [(x-1, y-1, ord(' ')), (x, y-1, ord(' ')), (x+1, y-1, ord(' ')),
